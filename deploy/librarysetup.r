35cb7073d8168b17581dfd577aef7d74cf982fce
cchoirat <cchoirat@gmail.com>
2014-12-07 11:54:45 -0500

    Install knitr, Zelig and dependencies

--- deploy/librarysetup.r
+++ deploy/librarysetup.r
@@ -1,6 +1,26 @@
-packageList<-c("knitr")
-for(i in 1:length(packageList)){
-    if (!require(packageList[i],character.only = TRUE)){
-        install.packages(packageList[i], repos=c("http://lib.stat.cmu.edu/R/CRAN/", "http://cran.wustl.edu", "http://cran.us.r-project.org"))
+packageList <- c("knitr",
+                 "devtools",
+                 "sandwich",
+                 "methods",
+                 "MASS",
+                 "survival",
+                 "VGAM",
+                 "jsonlite",
+                 "AER",
+                 "plyr",
+                 "dplyr",
+                 "quantreg",
+                 "geepack",
+                 "MCMCpack")
+
+for (i in 1:length(packageList)) {
+    if (!require(packageList[i], character.only = TRUE)) {
+        install.packages(packageList[i], repos = c("http://lib.stat.cmu.edu/R/CRAN/",
+                                                   "http://cran.wustl.edu",
+                                                   "http://cran.us.r-project.org"))
     }
 }
+
+library(devtools)
+devtools::install_github("IQSS/Zelig")
+devtools::install_github("IQSS/ZeligChoice")
