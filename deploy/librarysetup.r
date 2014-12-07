433d203791a02becf59dd1c895536361183991fb
Philip Durbin <philip_durbin@harvard.edu>
2014-12-06 23:03:09 -0500

    run librarysetup.r and knitall.R

--- deploy/librarysetup.r
+++ deploy/librarysetup.r
@@ -4,8 +4,3 @@ for(i in 1:length(packageList)){
         install.packages(packageList[i], repos=c("http://lib.stat.cmu.edu/R/CRAN/", "http://cran.wustl.edu", "http://cran.us.r-project.org"))
     }
 }
-update.packages(ask = FALSE, dependencies = c('Suggests'), oldPkgs=packageList, repos=c("http://lib.stat.cmu.edu/R/CRAN/", "http://cran.wustl.edu", "http://cran.us.r-project.org"))
-
-library(devtools)
-devtools::install_github("jcheng5/leaflet-shiny")
-
--- deploy/setup/stage1.sh
+++ deploy/setup/stage1.sh
@@ -12,7 +12,7 @@ rpm -Uvh http://dl.fedoraproject.org/pub/epel/6Server/x86_64/epel-release-6-8.no
 rpm --import http://ftp.scientificlinux.org/linux/scientific/6.4/x86_64/os/RPM-GPG-KEY-sl
 yum install -y http://ftp.scientificlinux.org/linux/scientific/6.4/x86_64/external_products/softwarecollections/yum-conf-softwarecollections-1.0-1.el6.noarch.rpm
 # httpd24 because we may want python27-mod_wsgi some day for Django
-yum install -y python27 python27-mod_wsgi httpd24 ack elinks
+yum install -y python27 python27-mod_wsgi httpd24 ack elinks R-core R-devel
 echo "Installing pip for Python 2.7"
 scl enable python27 "easy_install pip"
 echo "Install virtualenvwrapper"
@@ -25,6 +25,7 @@ mkdir /webapps/code
 chown plaid /webapps/code
 
 su plaid -l -s /bin/sh -c 'cd /webapps/code && cp -r /git/zeligproject.org .'
+/webapps/code/zeligproject.org/deploy/update-R-packages
 cp /webapps/code/zeligproject.org/deploy/files/etc/sudoers.d/plaid /etc/sudoers.d
 chmod 640 /etc/sudoers.d/plaid
 
