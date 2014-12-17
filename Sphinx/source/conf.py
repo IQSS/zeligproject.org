5fdf1bb7ee778f6a895b468ad36324ec219aa4f9
Michael Heppler <mheppler@hmdc.harvard.edu>
2014-12-17 11:34:00 -0500

    Fixed link for Zelig Project in navbar. Removed Models from About dropdown in navbar. Formatted the footer.

--- Sphinx/source/conf.py
+++ Sphinx/source/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Zelig'
-copyright = u'2014 The President and Fellows of Harvard College'
+copyright = u'2014, The President & Fellows of Harvard College'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
