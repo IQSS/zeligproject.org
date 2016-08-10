c4590e083f6b24ed2d3fcf424efd15d14584b666
cchoirat <cchoirat@gmail.com>
2016-08-10 15:39:17 -0400

    Install ZeligMultilevel from dev branch

--- deploy/librarysetup.r
+++ deploy/librarysetup.r
@@ -50,5 +50,5 @@ devtools::install_github("IQSS/Zelig")
 devtools::install_github("IQSS/ZeligChoice")
 devtools::install_github("IQSS/ZeligGAM")
 devtools::install_github("IQSS/ZeligEI")
-devtools::install_github("IQSS/ZeligMultilevel")
+devtools::install_github("IQSS/ZeligMultilevel", ref = "dev")
 
